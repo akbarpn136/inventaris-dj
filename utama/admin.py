ba31a151f729d59bc4611a51f356e47cf5e909ea
akbarpn136 <akbarpn136@gmail.com>
2015-12-16 10:41:48 +0700

    menambahkan fungsionalitas site admin

--- utama/admin.py
+++ utama/admin.py
@@ -2,5 +2,15 @@ from django.contrib import admin
 from .models import Ruangan, Kategori, Peralatan
 
 
+class PeralatanInline(admin.StackedInline):
+    model = Peralatan
+    extra = 0
+
+
+class KategoriAdmin(admin.ModelAdmin):
+    inlines = [PeralatanInline]
+
+
 # Register your models here.
 admin.site.register(Ruangan)
+admin.site.register(Kategori, KategoriAdmin)
