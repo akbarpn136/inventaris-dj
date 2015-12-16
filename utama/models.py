12daf19a1fecad268162535f4f96f3a276b124db
akbarpn136 <akbarpn136@gmail.com>
2015-12-16 10:27:20 +0700

    membuat model database

--- utama/models.py
+++ utama/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+
 # Create your models here.
+class Ruangan(models.Model):
+    nama_ruang = models.CharField(max_length=100, verbose_name='Nama Ruangan')
+
+
+class Kategori(models.Model):
+    nama_kategori = models.CharField(max_length=200, verbose_name='Kategori')
+
+
+class Peralatan(models.Model):
+    nama_alat = models.CharField(max_length=200, verbose_name='Tipe/Merk')
+    ip_alat = models.GenericIPAddressField(verbose_name='IP', blank=True, null=True)
+    personal_alat = models.ForeignKey('auth.User', verbose_name='Penanggung Jawab')
+    kategori_alat = models.ForeignKey(Kategori)
+    ruang_alat = models.ForeignKey(Ruangan)
