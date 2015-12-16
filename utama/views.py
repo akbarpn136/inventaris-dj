3647c6b6cc6e988b5fe65c3fd2dc02380cac6360
akbarpn136 <akbarpn136@gmail.com>
2015-12-16 11:47:55 +0700

    menambahkan fungsi tampil halaman daftar peralatan

--- utama/views.py
+++ utama/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Peralatan
+
 
 # Create your views here.
+def utama(request):
+    data_alat = Peralatan.objects.select_related().order_by('-kategori_alat')
+    return render(request, 'utama/daftar_peralatan.html', {'Peralatan': data_alat})
