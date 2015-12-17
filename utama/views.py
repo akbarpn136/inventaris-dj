9ea351acb28e298a9fa8c87aac8cb225f772fd40
akbarpn136 <akbarpn136@gmail.com>
2015-12-17 10:04:36 +0700

    tambah migrasi model database

--- utama/views.py
+++ utama/views.py
@@ -5,4 +5,18 @@ from .models import Peralatan
 # Create your views here.
 def utama(request):
     data_alat = Peralatan.objects.select_related().order_by('-kategori_alat')
-    return render(request, 'utama/daftar_peralatan.html', {'Peralatan': data_alat})
+    data_komputer = Peralatan.objects.filter(kategori_alat__nama_kategori='Komputer')
+    data_printer = Peralatan.objects.filter(kategori_alat__nama_kategori='Printer')
+
+    data_komputer_rusak = data_komputer.filter(kondisi_alat=False)
+    data_printer_rusak = data_printer.filter(kondisi_alat=False)
+
+    data = {
+        'Peralatan': data_alat,
+        'komputer': data_komputer,
+        'printer': data_printer,
+        'komputer_rusak': data_komputer_rusak,
+        'printer_rusak': data_printer_rusak,
+    }
+
+    return render(request, 'utama/daftar_peralatan.html', data)
