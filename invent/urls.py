6dd39627a404b7519ed54f59c0a7a67d5933f039
akbarpn136 <akbarpn136@gmail.com>
2016-03-23 15:06:08 +0700

    konfigurasi lanjut dengan menambahkan pola url aplikasi reformasi kedalam aplikasi induk/utama

--- invent/urls.py
+++ invent/urls.py
@@ -19,5 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('utama.urls')),
-    url(r'^reformasi/', include('utama.urls')),
+    url(r'^reformasi/', include('reformasi.urls')),
 ]
