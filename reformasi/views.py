8ed2bd5c8c037a23e17a9882a9bc2758c7627595
akbarpn136 <akbarpn136@gmail.com>
2016-03-23 16:03:27 +0700

    konfigurasi lanjut dengan memasukkan data dari model database dalam fungsi index

--- reformasi/views.py
+++ reformasi/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 
+from .models import KumpulanDokumen
+
 
 # Create your views here.
 def index(request):
-    data = {}
+    data_dokumen = KumpulanDokumen.objects.all()
+    data = {
+        'dokumen': data_dokumen
+    }
 
     return render(request, 'reformasi/daftar_reformasi.html', data)
