e4c89fcf776cae0976db942ee60c560b11d83581
akbarpn136 <akbarpn136@gmail.com>
2016-03-23 15:19:15 +0700

    konfigurasi lanjut dengan menambahkan model database untuk menyimpan data dokumen

--- reformasi/models.py
+++ reformasi/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.core.validators import URLValidator
+
 
 # Create your models here.
+class KumpulanDokumen(models.Model):
+    nama_dokumen = models.TextField(verbose_name='Nama dokumen')
+    kategori_dokumen = models.CharField(verbose_name='Kategori dokumen', max_length=100)
+    url_dokumen = models.TextField(verbose_name='URL dokumen', validators=[URLValidator()])
+
+    def __str__(self):
+        return self.nama_dokumen
