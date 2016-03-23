4c2ff4d0d0ae514bd40550a4ad936219a42cb44d
akbarpn136 <akbarpn136@gmail.com>
2016-03-23 14:40:24 +0700

    konfigurasi awal struktur aplikasi daftar pemenuhan persyaratan reformasi birokrasi

--- invent/urls.py
+++ invent/urls.py
@@ -19,4 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('utama.urls')),
+    url(r'^reformasi/', include('utama.urls')),
 ]
